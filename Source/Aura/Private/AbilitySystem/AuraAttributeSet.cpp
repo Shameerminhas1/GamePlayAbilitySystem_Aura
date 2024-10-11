d113615488dcc3066877e886b799d03bf853abb3
Shameerminhas1 <122668054+Shameerminhas1@users.noreply.github.com>
2024-10-11 15:41:15 +0500

    Modifier Order of Operations

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -10,7 +10,7 @@
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-	InitHealth(50.f);                             
+	InitHealth(10.f);                             
 	InitMaxHealth(100.f);
 	InitMana(10.f);
 	InitMaxMana(50.f);
@@ -37,11 +37,11 @@ void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, 
 	// value return from querying modifier - which can be more then what is clamped as it comes from modifier - &NewValue
 	if (Attribute == GetHealthAttribute())
 	{
-		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+		//NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
 	}
 	if (Attribute == GetManaAttribute())
 	{
-		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+		//NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
 	}
 }
 
@@ -88,13 +88,13 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
 	{
 		//setting health - a proper clamp
-		SetHealth(FMath::Clamp(GetHealth(),0.f,GetMaxHealth()));
+	//	SetHealth(FMath::Clamp(GetHealth(),0.f,GetMaxHealth()));
 	}
 
 	if (Data.EvaluatedData.Attribute == GetManaAttribute())
 	{
 		//setting Mana - a proper clamp
-		SetMana(FMath::Clamp(GetMana(),0.f,GetMaxMana()));
+	//	SetMana(FMath::Clamp(GetMana(),0.f,GetMaxMana()));
 	}
 }
 
