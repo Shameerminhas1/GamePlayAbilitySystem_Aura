df714934dfaf352ba7278b01d4abf11fa4169239
Shameerminhas1 <122668054+Shameerminhas1@users.noreply.github.com>
2024-10-16 15:39:53 +0500

    Attributeinfo - datasset Array

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,22 @@
+// copyright Game Strix Studio
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoFromTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for (const FAuraAttributeInfo& Info : AttributeInformation)
+	{
+		if (Info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return Info;
+		}
+	}
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find information about ATTRIBUTE TAG [%s] on Array ATTRIBUTEINFO [%s]"),*AttributeTag.ToString(),*GetNameSafe(this));
+	}
+
+	return  FAuraAttributeInfo();
+}
