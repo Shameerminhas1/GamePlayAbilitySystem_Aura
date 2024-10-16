9af4748bc06830c0eed14931302462afa066e3ce
Shameerminhas1 <122668054+Shameerminhas1@users.noreply.github.com>
2024-10-16 16:31:55 +0500

    Attributemenu - widgetcontroller class - mk

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// copyright Game Strix Studio
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
