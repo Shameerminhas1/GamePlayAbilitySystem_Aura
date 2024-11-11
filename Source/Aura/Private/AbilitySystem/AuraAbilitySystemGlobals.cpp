f7d8b9a126a8718105533d5b38c08016bfdbfe0f
Shameerminhas1 <122668054+Shameerminhas1@users.noreply.github.com>
2024-11-11 13:20:14 +0500

    aura ability system globals

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,12 @@
+// copyright Game Strix Studio
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext;
+	
+}
