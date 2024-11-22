5bffcaea3790d95d017eb6c16eafae46376471f3
Shameerminhas1 <122668054+Shameerminhas1@users.noreply.github.com>
2024-11-22 09:27:48 +0500

    Deac blackboard Key

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -79,6 +79,7 @@ AActor* AAuraEnemy::GetCombatTarget_Implementation() const
 void AAuraEnemy::Die()
 {
 	SetLifeSpan(LifeSpan);
+	if(AuraAIController) AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("Dead"),true);
 	Super::Die();
 }
 
