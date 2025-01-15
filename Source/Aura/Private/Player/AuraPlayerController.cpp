aae863fb40950e4be2189530a41ac79d17741213
Shameerminhas1 <122668054+Shameerminhas1@users.noreply.github.com>
2025-01-15 17:14:26 +0500

    Magic Circle Interface Funvtion

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -14,6 +14,7 @@
 #include "NavigationPath.h"
 #include "NiagaraFunctionLibrary.h"
 #include "Actor/MagicCircle.h"
+#include "Components/DecalComponent.h"
 #include "GameFramework/Character.h"
 #include "UI/Widget/DamageTextComponent.h"
 
@@ -53,12 +54,16 @@ void AAuraPlayerController::PlayerTick(float DeltaTime)
 	UpdateMagicCircleLocation();
 }
 
-void AAuraPlayerController::ShowMagicCircle()
+void AAuraPlayerController::ShowMagicCircle(UMaterialInterface* DecalMaterial)
 {
 	if (!IsValid(MagicCircle))
-    	{
-    		MagicCircle = GetWorld()->SpawnActor<AMagicCircle>(MagicCircleClass);
-    	}
+    {
+		MagicCircle = GetWorld()->SpawnActor<AMagicCircle>(MagicCircleClass);
+		if (DecalMaterial)
+		{
+		    MagicCircle->MagicCircleDecal->SetMaterial(0,DecalMaterial);
+		}
+    }
 }
 
 void AAuraPlayerController::HideMagicCircle()
--- Source/Aura/Public/Actor/MagicCircle.h
+++ Source/Aura/Public/Actor/MagicCircle.h
@@ -17,12 +17,14 @@ public:
 
 	virtual void Tick(float DeltaTime) override;
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+    TObjectPtr<UDecalComponent> MagicCircleDecal;
+
 protected:
 	
 	virtual void BeginPlay() override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	TObjectPtr<UDecalComponent> MagicCircleDecal;
+	
 	
 
 };
