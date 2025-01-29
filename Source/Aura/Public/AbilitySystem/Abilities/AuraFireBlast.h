0adacdf307f6cd3caf65f5e532351f0e325259c7
Shameerminhas1 <122668054+Shameerminhas1@users.noreply.github.com>
2025-01-29 15:49:22 +0500

    FireBlast Ability

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraFireBlast.h
@@ -0,0 +1,26 @@
+// copyright Game Strix Studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "AuraFireBlast.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraFireBlast : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+public:
+
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString	GetNextLevelDescription(int32 Level) override;
+
+	protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="FireBlast")
+	int32 NumFireballs = 12;
+	
+};
